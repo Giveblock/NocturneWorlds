6a9fc5bf0c415b8a6959e89ec5baaf860e60bcd3
BuildTools <unconfigured@null.spigotmc.org>
2025-03-29 13:47:16 -0400

    commit

--- src/main/java/me/giveblock/nocturneWorlds/utils/Json.java
+++ src/main/java/me/giveblock/nocturneWorlds/utils/Json.java
@@ -1,4 +1,24 @@
 package me.giveblock.nocturneWorlds.utils;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Json {
+
+    public static JsonObject load(File file) {
+        try {
+            String data = new String(Files.readAllBytes(Paths.get(file.toURI())));
+            return JsonParser.parseString(data).getAsJsonObject();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
