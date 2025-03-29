e6ad9f01cf2cc40a9ab0a62adc3d2b45ea19ff8f
BuildTools <unconfigured@null.spigotmc.org>
2025-03-29 13:40:45 -0400

    commit

---
+++ src/main/java/me/giveblock/nocturneWorlds/NocturneWorlds.java
@@ -0,0 +1,17 @@
+package me.giveblock.nocturneWorlds;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NocturneWorlds extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
